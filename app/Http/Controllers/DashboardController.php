21557d05123f7351aa139721344ab93d62662d04
IrshamAja <m.irsham98@gmail.com>
2022-12-11 21:35:37 +0800

    desain dashboard dan count data general

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        // cari jumlah user
+        $userCount = User::count();
+        // cari jumlah category
+        $categoryCount = Category::count();
+        // cari jumlah book
+        $bookCount = Book::count();
+
+        return view('dashboard', [
+            'book_count' => $bookCount,
+            'category_count' => $categoryCount,
+            'user_count' => $userCount
+        ]);
     }
 }
