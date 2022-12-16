0afb015cb99616b6f96e2e565fc3f6add9fab018
IrshamAja <m.irsham98@gmail.com>
2022-12-16 13:49:40 +0800

    perbaiki error search

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -17,15 +17,25 @@ class ClientController extends Controller
     public function bookList(Request $request)
     {
         $categories = Category::all();
-        $title = $request->title;
-        $cat = $request->category;
+        // $title = $request->title;
+        // $cat = $request->category;
 
         $books = Book::with('categories')->get();
-        if ($title || $cat) {
-            $books =Book::with(['categories','cats'])->where('title', 'LIKE', "%{$title}%")
-                    ->orWhereHas('categories', function($q) use($cat){
-                        $q->where('categories.id', $cat);
-                        })->get();
+        // if ($title || $cat) {
+        //     $books =Book::with(['categories','cats'])->where('title', 'LIKE', "%{$title}%")
+        //             ->orWhereHas('categories', function($q) use($cat){
+        //                 $q->where('categories.id', $cat);
+        //                 })->get();
+        // }
+
+        if ($request->title) {
+            $books =Book::where('title', 'LIKE', '%'.$request->title.'%')
+                        ->get();
+        }
+        elseif ($request->category) {
+            $books = Book::whereHas('categories', function($query) use($request) {
+                $query->where('categories.id', $request->category);
+            })->get();
         }
         return view('book-list-user', [
             'books' => $books,
